c87aaecc749d8c6a05134965a5330a32c4303b06
jhole89 <joel.lutman@gmail.com>
2019-09-07 21:25:21 +0100

    add http example

---
+++ src/04_http.elm
@@ -0,0 +1,46 @@
+import Browser
+import Html exposing (Html, text, pre)
+import Http
+
+main = Browser.element { init = init,  view = view , update = update, subscriptions = subs }
+
+type Model
+  = Failure
+  | Loading
+  | Success String
+
+init: () -> (Model, Cmd Msg)
+init _ =
+    ( Loading
+    , Http.get
+      { url = "https://elm-lang.org/assets/public-opinion.txt"
+      , expect = Http.expectString GotText
+      }
+    )
+
+type Msg
+  = GotText (Result Http.Error String)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg _ =
+  case msg of
+      GotText result ->
+          case result of
+              Ok fullText ->
+                  (Success fullText, Cmd.none)
+              Err _ ->
+                  (Failure, Cmd.none)
+
+subs: Model -> Sub Msg
+subs _ =
+    Sub.none
+
+view: Model -> Html Msg
+view model =
+    case model of
+        Failure ->
+            text "I was unable to load your book."
+        Loading ->
+            text "Loading..."
+        Success fullText ->
+            pre [] [ text fullText ]
