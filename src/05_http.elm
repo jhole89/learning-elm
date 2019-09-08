a685011bc08ecab70e716ad91f6737cf81b8c642
jhole89 <joel.lutman@gmail.com>
2019-09-08 10:32:49 +0100

    formatting

--- src/05_http.elm
+++ src/05_http.elm
@@ -1,46 +1,59 @@
+module Main exposing (Model(..), Msg(..), init, main, subs, update, view)
+
 import Browser
-import Html exposing (Html, text, pre)
+import Html exposing (Html, pre, text)
 import Http
 
-main = Browser.element { init = init,  view = view , update = update, subscriptions = subs }
+
+main =
+    Browser.element { init = init, view = view, update = update, subscriptions = subs }
+
 
 type Model
-  = Failure
-  | Loading
-  | Success String
+    = Failure
+    | Loading
+    | Success String
 
-init: () -> (Model, Cmd Msg)
+
+init : () -> ( Model, Cmd Msg )
 init _ =
     ( Loading
     , Http.get
-      { url = "https://elm-lang.org/assets/public-opinion.txt"
-      , expect = Http.expectString GotText
-      }
+        { url = "https://elm-lang.org/assets/public-opinion.txt"
+        , expect = Http.expectString GotText
+        }
     )
 
+
 type Msg
-  = GotText (Result Http.Error String)
+    = GotText (Result Http.Error String)
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg _ =
-  case msg of
-      GotText result ->
-          case result of
-              Ok fullText ->
-                  (Success fullText, Cmd.none)
-              Err _ ->
-                  (Failure, Cmd.none)
+    case msg of
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    ( Success fullText, Cmd.none )
 
-subs: Model -> Sub Msg
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+
+subs : Model -> Sub Msg
 subs _ =
     Sub.none
 
-view: Model -> Html Msg
+
+view : Model -> Html Msg
 view model =
     case model of
         Failure ->
             text "I was unable to load your book."
+
         Loading ->
             text "Loading..."
+
         Success fullText ->
             pre [] [ text fullText ]
